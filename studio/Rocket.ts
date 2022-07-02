c7c07cfcd2377458bfe385d81dff4526a0eba3bc
Sean Duffy <kqrpnb@gmail.com>
2022-07-02 06:01:31 -0400

    Nailed it

---
+++ studio/Rocket.ts
@@ -0,0 +1,50 @@
+import { Astronaut } from "./Astronaut";
+import { Cargo } from "./Cargo";
+import { Payload } from "./Payload";
+
+export class Rocket {
+    name: string;
+    totalCapacityKg: number;
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+
+    constructor(name: string, totalCapacityKg: number) {
+        this.name = name;
+        this.totalCapacityKg = totalCapacityKg;
+    }
+
+    sumMass(items: Payload[] ): number {
+        let total: number = 0;
+        items.forEach((element) => {
+            total += element.massKg;
+        });
+        // for (let i: number = 0; i < items.length; i++) {
+        //     total = total + items[i].massKg;
+        // }
+        return total;
+    }
+
+    currentMassKg(): number {
+        return this.sumMass(this.astronauts) + this.sumMass(this.cargoItems);
+    }
+
+    canAdd(item: Payload): boolean {
+        return ((this.currentMassKg() + item.massKg) <= this.totalCapacityKg);
+    }
+
+    addCargo(cargo: Cargo): boolean {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        return false;
+    }
+
+    addAstronaut(astronaut: Astronaut): boolean {
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        return false;
+    }
+}
